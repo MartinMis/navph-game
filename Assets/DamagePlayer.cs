ae116d04ad0f47cdc6f2e2c75f2206dad39ddf5d
Martin Mislovič <martin.mislovic@gmail.com>
2024-11-08 23:28:04 +0100

    Add damage to windows
    This approach might require optimisation in the future

--- Assets/DamagePlayer.cs
+++ Assets/DamagePlayer.cs
@@ -1,27 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
+[RequireComponent(typeof(Light2D))]
 public class DamagePlayer : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float damage;
+    [SerializeField] private int rayCount;
+    
+    private Light2D _light;
+    private float _startAngle;
+    private float _angleStep;
 
-    // Update is called once per frame
+    void Start()
+    {   
+        _light = GetComponent<Light2D>();
+        _angleStep = _light.pointLightOuterAngle/rayCount;
+        _startAngle = _light.pointLightOuterAngle/2;
+    }
+    
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, playerTransform.position - transform.position, Mathf.Infinity);
-        if (hit.collider != null)
+        List<Vector3> rayDirections = new List<Vector3>();
+        for (int i = 0; i <= rayCount; i++)
         {
-            if (hit.collider.CompareTag("Player"))
+            Vector3 rayDirection = Quaternion.AngleAxis(_startAngle - i * _angleStep, new Vector3(0,0,1)) * transform.up;
+            rayDirections.Add(rayDirection);
+            Debug.DrawRay(transform.position,  rayDirection *  _light.pointLightOuterRadius,Color.red);
+        }
+        
+
+        foreach (Vector3 rayDirection in rayDirections)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection, _light.pointLightOuterRadius);
+            if (hit.collider)
             {
-                Debug.Log("Hit Player");
-                hit.collider.transform.Rotate(new Vector3(0, 0, 1), 10 * Time.deltaTime);
+                if (hit.collider.CompareTag("Player"))
+                {
+                    hit.collider.GetComponent<PlayerController>().DamagePlayer(damage + Time.deltaTime);
+                }
             }
+            
         }
     }
 }
