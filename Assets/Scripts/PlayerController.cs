bef0c3f5f3495b727f0c8a3ae44d9ea51bbffa14
Martin Mislovič <martin.mislovic@gmail.com>
2024-11-09 01:06:04 +0100

    Add basic mobile movement
    The movement system will probably need good cause its not great

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovementController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float health = 0;
+    [SerializeField] private TextMeshProUGUI wakeupMeter;
+    [SerializeField] private float speed ;
     // Start is called before the first frame update
+    
+    private Rigidbody2D _rigidbody;
+    private Vector3 _moveInput;
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        wakeupMeter.text = "Wakeup Meter: " + health;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movementVector = new Vector3(moveHorizontal, moveVertical, 0);
-        transform.Translate(movementVector * movementSpeed * Time.deltaTime);
+        _rigidbody.velocity = _moveInput * speed;
+    }
+
+    public void DamagePlayer(float damage)
+    {
+        health += damage;
+        wakeupMeter.text = "Wakeup Meter: " + health;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
     }
 }
