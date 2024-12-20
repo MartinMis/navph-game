ad47540865b3c8cf6c9ca82905faffc484f50cc5
star1master <hitka60@gmail.com>
2024-12-20 15:26:37 +0100

    added stack of pills

---
+++ Assets/Scripts/HealPlayer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class HealPlayer: MonoBehaviour
+{
+    [SerializeField] private float healAmount = 10f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerController playerController = collision.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.Heal(healAmount);
+            }
+            else
+            {
+                Debug.LogError("PlayerController script not found on the player.");
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
